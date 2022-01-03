f68fda0a83597a27f055d7d89c12db27aea89f0e
awebisam <abisamism@gmail.com>
2022-01-03 14:03:44 +0545

    create basic model structure

--- backend/app_profile/models.py
+++ backend/app_profile/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
-# Create your models here.
+
+class Profile(models.Model):
+
+    class GenderChoice(models.TextChoices):
+
+        MALE = 'M', 'Male'
+        FEMALE = 'F', 'Female'
+        OTHER = 'O', 'Other'
+
+    class ContactPreference(models.TextChoices):
+
+        EMAIL = 'E', 'Email'
+        PHONE = 'P', 'Phone'
+        NONE = 'N', 'None'
+
+    name = models.CharField(max_length=50)
+    gender = models.CharField(max_length=10, choices=GenderChoice.choices)
+    phone = models.CharField(max_length=20)
+    email = models.EmailField(max_length=254)
+    address = models.CharField(max_length=100)
+    nationality = models.CharField(max_length=50)
+    dob = models.DateField()
+    educational_background = models.CharField(max_length=50)
+    preffered_contact = models.CharField(
+        max_length=10, choices=ContactPreference.choices, default=ContactPreference.NONE)
+
+    class Meta:
+        verbose_name = "Profile"
+        verbose_name_plural = "Profiles"
+
+    def __str__(self):
+        return self.name
