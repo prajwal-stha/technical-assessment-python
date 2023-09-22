e8d4d7ff1ad2d25fd26eae3e89ce85794109f434
Anjaan <anjaangaire@gmail.com>
2023-09-22 20:54:48 +0545

    project setup with different settings for different environments

---
+++ src/settings/__init__.py
@@ -0,0 +1,15 @@
+from decouple import config
+
+from .base import *
+
+ENV = config("ENVIRONMENT")
+
+if ENV == "LOCAL":
+    from .local import *
+elif ENV == "STAG":
+    from .stag import *
+elif ENV == "PROD":
+    from .prod import *
+else:
+    print("Invalid Environment Selected !!! Check ENVIRONMENT variable in your .env")
+    exit(1)
