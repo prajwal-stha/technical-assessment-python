d0059a75d6be735fdf13e76d7b6a233bfd4b2f86
Anjaan <anjaangaire@gmail.com>
2023-09-22 20:56:00 +0545

    pre-commit hooks initialized

--- src/settings/local.py
+++ src/settings/local.py
@@ -3,15 +3,15 @@ import os
 from .base import *
 
 ALLOWED_HOSTS = [
-    "*",
+    '*',
 ]
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
-STATIC_URL = "/static/"
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
