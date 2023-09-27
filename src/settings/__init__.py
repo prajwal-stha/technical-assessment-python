e5321854aa0197fdfb277e5260a294dfc525d735
Anjaan <anjaangaire@gmail.com>
2023-09-27 15:02:07 +0545

    settings updated for production and staging environments

--- src/settings/__init__.py
+++ src/settings/__init__.py
@@ -2,14 +2,14 @@ from decouple import config
 
 from .base import *
 
-ENV = config('ENVIRONMENT')
+ENV = config("ENVIRONMENT")
 
-if ENV == 'LOCAL':
+if ENV == "LOCAL":
     from .local import *
-elif ENV == 'STAG':
+elif ENV == "STAG":
     from .stag import *
-elif ENV == 'PROD':
+elif ENV == "PROD":
     from .prod import *
 else:
-    print('Invalid Environment Selected !!! Check ENVIRONMENT variable in your .env')
+    print("Invalid Environment Selected !!! Check ENVIRONMENT variable in your .env")
     exit(1)
